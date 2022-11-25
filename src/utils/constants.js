e50265f955c14bb77f6cb27e585d65918ead0766
Sarthak Nigam <77585810+curio-7@users.noreply.github.com>
2022-11-25 16:20:37 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -37,7 +37,7 @@ export const categories = [
 
 export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
 export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
-export const demoVideoUrl = '/video/GDa8kZLNhJ4';
-export const demoChannelTitle = 'JavaScript Mastery';
-export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoVideoUrl = '/video/5Eqb_-j3FDA';
+export const demoChannelTitle = 'Coke Studio';
+export const demoVideoTitle = 'Pasoori';
 export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
